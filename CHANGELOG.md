e65e0a3e4aa76ed0811572b0e0988e9fe38a18a1
Tiago Barroso <tmbb@campus.ul.pt>
2026-02-21 11:38:09 +0000

    Version release 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 # Changelog
 
 <!-- %% CHANGELOG_ENTRIES %% -->
+
+## 0.2.0 - 2026-02-21
+
+- First public version
+- One test is skipped because it always fails, probably due to a bug in the `:req_cassette` library.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geminix.MixProject do
   def project do
     [
       app: :geminix,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.19",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
